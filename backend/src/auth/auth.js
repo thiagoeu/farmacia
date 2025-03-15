f73d702142966d2cafc77d4e0e9c2f762031b9dd
thiagoeu <araujo.thiago1051@gmail.com>
2025-03-14 22:15:32 -0300

    feat: auth

--- backend/src/auth/auth.js
+++ backend/src/auth/auth.js
@@ -3,115 +3,171 @@ import passport from "passport";
 import LocalStrategy from "passport-local";
 import crypto from "crypto";
 import jwt from "jsonwebtoken";
-
 import { Mongo } from "../database/mongo.js";
 import { ObjectId } from "mongodb";
 
 const collectionName = "users";
+const authRouter = express.Router();
 
+// Configuração do Passport para autenticação local
 passport.use(
   new LocalStrategy(
-    { userNameField: "email" },
-    async (email, password, callback) => {
-      const user = await Mongo.db
-        .collection(collectionName)
-        .findOne({ email: email });
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await Mongo.db
+          .collection(collectionName)
+          .findOne({ email });
 
-      if (!user) {
-        return callback(null, false);
-      }
-
-      const saltBuffer = user.salt.saltBuffer;
-
-      crypto.pbkdf2(
-        password,
-        saltBuffer,
-        310000,
-        16,
-        "sha256",
-        (err, hashedPassword) => {
-          if (err) {
-            return callback(null, false);
-          }
-
-          const userPasswordBuffer = Buffer.from(user.password.buffer);
-
-          if (!crypto.timingSafeEqual(userPasswordBuffer, hashedPassword)) {
-            return callback(null, false);
-          }
-
-          const { password, salt, ...rest } = user;
-
-          return callback(null, rest);
+        if (!user) {
+          return done(null, false, { message: "Invalid email or password" });
         }
-      );
+
+        const saltBuffer = Buffer.from(user.salt); // Converte o salt armazenado para Buffer
+        const storedPasswordBuffer = Buffer.from(user.password); // Converte a senha armazenada para Buffer
+
+        // Gerar hash da senha fornecida para comparação
+        crypto.pbkdf2(
+          password,
+          saltBuffer,
+          310000,
+          storedPasswordBuffer.length,
+          "sha256",
+          (err, hashedPassword) => {
+            if (err) return done(err);
+
+            const hashedPasswordBuffer = Buffer.from(hashedPassword); // Converte o hash gerado para Buffer
+
+            // Verifica se os buffers têm o mesmo tamanho antes da comparação
+            if (storedPasswordBuffer.length !== hashedPasswordBuffer.length) {
+              return done(null, false, {
+                message: "Invalid email or password",
+              });
+            }
+
+            if (
+              !crypto.timingSafeEqual(
+                storedPasswordBuffer,
+                hashedPasswordBuffer
+              )
+            ) {
+              return done(null, false, {
+                message: "Invalid email or password",
+              });
+            }
+
+            const { password, salt, ...userWithoutPassword } = user;
+            return done(null, userWithoutPassword);
+          }
+        );
+      } catch (error) {
+        return done(error);
+      }
     }
   )
 );
 
-const authRouter = express.Router();
-
+// Rota de cadastro de usuário
 authRouter.post("/signup", async (req, res) => {
-  const checkUser = await Mongo.db
-    .collection(collectionName)
-    .findOne({ email: req.body.email });
+  try {
+    const { fullname, email, password } = req.body;
 
-  if (checkUser) {
-    return res.status(500).send({
-      success: false,
-      statusCode: 500,
-      body: {
-        text: "usuario já existe",
-      },
-    });
-  }
-
-  const salt = crypto.randomBytes(16);
-  crypto.pbkdf2(
-    req.body.password,
-    salt,
-    310000,
-    16,
-    "sha256",
-    async (err, hashedPassword) => {
-      if (err) {
-        return res.status(500).send({
-          success: false,
-          statusCode: 500,
-          body: {
-            text: "erro na crypto password!",
-            err: err,
-          },
-        });
-      }
-      const result = await Mongo.db.collection(collectionName).insertOne({
-        email: req.body.email,
-        password: hashedPassword,
-        salt,
-      });
-
-      if (result.insertedId) {
-        const user = await Mongo.db
-          .collection(collectionName)
-          .findOne({ _id: new ObjectId(result.insertedId) });
-
-        const token = jwt.sign({ id: user._id, email: user.email }, "secret", {
-          expiresIn: "1h",
-        });
-
-        return res.send({
-          success: true,
-          statusCode: 200,
-          body: {
-            text: "usuario registrado corretamente",
-            token,
-            user,
-            logged: true,
-          },
-        });
-      }
+    // Verifica se o usuário já existe
+    const existingUser = await Mongo.db
+      .collection(collectionName)
+      .findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
     }
-  );
+
+    // Gera um salt e hasheia a senha
+    const salt = crypto.randomBytes(16);
+
+    crypto.pbkdf2(
+      password,
+      salt,
+      310000,
+      32,
+      "sha256",
+      async (error, hashedPassword) => {
+        if (error) {
+          return res
+            .status(500)
+            .json({ success: false, message: "Error hashing password", error });
+        }
+
+        // Insere o usuário no banco
+        const result = await Mongo.db.collection(collectionName).insertOne({
+          fullname,
+          email,
+          password: hashedPassword,
+          salt,
+        });
+
+        if (result.insertedId) {
+          const user = await Mongo.db
+            .collection(collectionName)
+            .findOne(
+              { _id: new ObjectId(result.insertedId) },
+              { projection: { password: 0, salt: 0 } }
+            );
+
+          const token = jwt.sign(
+            { id: user._id, email: user.email },
+            process.env.JWT_SECRET || "secret",
+            {
+              expiresIn: "1h",
+            }
+          );
+
+          return res.status(201).json({
+            success: true,
+            message: "User registered successfully",
+            user,
+            token,
+          });
+        }
+      }
+    );
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error", error });
+  }
+});
+
+// Rota de login
+authRouter.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err)
+      return res
+        .status(500)
+        .json({ success: false, message: "Internal Server Error", error: err });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email or password" });
+    }
+
+    // Gera um token para o usuário autenticado
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET || "secret",
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      user,
+      token,
+    });
+  })(req, res, next);
 });
 
 export default authRouter;
